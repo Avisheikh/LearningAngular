e08f3880068807376e4ad732f6758271ede53a39
abishek.bhandari@fonepay.com <abishek.bhandari@fonepay.com>
2022-05-26 13:43:32 +0545

    Custom validation has been finished

--- our-first-form/src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
+++ our-first-form/src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+function skuValidator(control:FormControl): { [s: string]: boolean } {
+  if(!control.value.match(/^123/)){
+    return {invalidSku: true};
+  }
+  return null as any;
+}
+
 @Component({
   selector: 'app-demo-form-with-validations-explicit',
   templateUrl: './demo-form-with-validations-explicit.component.html',
@@ -9,14 +16,14 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from
 export class DemoFormWithValidationsExplicitComponent implements OnInit {
 
   myForm:FormGroup;
-  sku:AbstractControl;
+  // sku:AbstractControl;
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      'sku':['',Validators.required]
+      'sku':['',Validators.compose([Validators.required, skuValidator])]
     });
 
-    this.sku = this.myForm.controls['sku'];
+    // this.sku = this.myForm.controls['sku'];
    }
 
   ngOnInit(): void {
