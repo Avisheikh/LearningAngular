8a462a37757bcffd3e01d03f9bf78d3a9ec08c36
abishek.bhandari@fonepay.com <abishek.bhandari@fonepay.com>
2022-05-27 14:51:25 +0545

    HTTP basice request has been completed

---
+++ http/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleHttpComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
